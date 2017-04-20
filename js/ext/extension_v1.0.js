c2a549fcf9dd1aeab8a2271b5dae5b78f27d6d49
csf30816 <elijahporter@zoho.com>
2017-04-20 12:25:54 -1000

    Update extension_v1.0.js

--- js/ext/extension_v1.0.js
+++ js/ext/extension_v1.0.js
@@ -1,1 +1,24 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.verify_acc = function() {
+        // Code that gets executed when the block is run
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'Check credentials %s %s', 'verify_acc', 'Username', 'Password'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('My first extension', descriptor, ext);
+})({});
