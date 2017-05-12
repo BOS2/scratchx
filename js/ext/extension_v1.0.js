8bc4e967d69ec1e411448cd816d1d71e36c379e6
csf30816 <elijahporter@zoho.com>
2017-05-12 12:30:22 -1000

    Update extension_v1.0.js

--- js/ext/extension_v1.0.js
+++ js/ext/extension_v1.0.js
@@ -1,44 +1,34 @@
-(function(ext) {
+(function (ext) {
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function () {};
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+    ext._getStatus = function () {
+        return {
+            status: 2,
+            msg: 'Ready'
+        };
     };
-    
-    function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
 
-    ext.verify_acc = function(username, password) {
-        var url = '//bankos.cf/api/v1?t=verify&u=' + username + '&p=' + password + '&callback=?';
-        $.getJSON(url, function(data) {
-            success: readData(data)
-        });
-        
-        function readData(data) {
-            alert(data.msg);
+    function fnv32a(str) {
+        var FNV1_32A_INIT = 0x811c9dc5;
+        var hval = FNV1_32A_INIT;
+        for (var i = 0; i < str.length; ++i) {
+            hval ^= str.charCodeAt(i);
+            hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
         }
+        return hval >>> 0;
+    }
+    
+    ext.hash = function (key) {
+        return fnv32a(key);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'Check credentials %s %s', 'verify_acc', 'Username', 'Password'],
+            ['R', 'Hash %s', 'hash', 'Key'],
         ]
     };
 
