a310a7c5bd95314860de6f512d8ede2aaafccc6a
csf30816 <elijahporter@zoho.com>
2017-05-12 15:54:58 -1000

    Update extension_v1.0.js

--- js/ext/extension_v1.0.js
+++ js/ext/extension_v1.0.js
@@ -1,1 +1,2 @@
 ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions/hash.js");
+ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/StringExt.js");
