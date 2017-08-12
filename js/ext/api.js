7336c9ee2774ceb8750bd2ceab975b16f1e6bc44
csf30816 <csf30816@users.noreply.github.com>
2017-08-12 11:48:25 -1000

    Update api.js

--- js/ext/api.js
+++ js/ext/api.js
@@ -1,7 +1,3 @@
-var sc = document.createElement("script");
-sc.setAttribute("src", "https://cloudboost.io/js-sdk/cloudboost.js");
-document.body.appendChild(sc);
-CB.CloudApp.init('ojzmkthcnnsn', '5c58312b-fcea-4173-9644-3870217903f2');
 (function (ext) {
 
 	// Cleanup function when the extension is unloaded
@@ -16,24 +12,13 @@ CB.CloudApp.init('ojzmkthcnnsn', '5c58312b-fcea-4173-9644-3870217903f2');
 		};
 	};
 
-	ext.grab = function (u, p, callback) {
-		var query = new CB.CloudQuery("info");
-		query.equalTo('username', u);
-		query.equalTo('password', p);
-		query.greaterThan('age', 10);
-		query.find({
-			success: function (list) {
-				return true;
-			},
-			error: function (error) {
-				return false;
-			}
-		});
+	ext.check = function (u, p, callback) {
+		
 	};
 	// Block and block menu descriptions
 	var descriptor = {
 		blocks: [
-			['R', 'Verify %s %s', 'grab'],
+			['R', 'Verify %s %s', 'check'],
 		],
 		url: 'https://bankos.cf'
 	};
