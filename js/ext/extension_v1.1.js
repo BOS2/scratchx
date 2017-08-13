96b283b723d4ad5a122f2a0ab1d5974572a87b22
csf30816 <csf30816@users.noreply.github.com>
2017-08-12 17:04:34 -1000

    Update extension_v1.1.js

--- js/ext/extension_v1.1.js
+++ js/ext/extension_v1.1.js
@@ -1,2 +1,27 @@
 ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions/hash.js");
 ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/StringExt.js");
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.my_first_block = function() {
+        return "false";
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['R', 'my first block', 'my_first_block'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('My first extension', descriptor, ext);
+})({});
